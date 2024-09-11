1a2d01db685fbd604f6b5c5d0a3ee4e122b980ee
Kerkar <kerkar.kevin@gmail.com>
2024-09-11 22:56:43 +0200

    Entity created

---
+++ src/Entity/Car.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $daily_price = null;
+
+    #[ORM\Column]
+    private ?float $monthly_price = null;
+
+    #[ORM\Column]
+    private ?bool $transmission = null;
+
+    #[ORM\Column]
+    private ?int $capacity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDailyPrice(): ?float
+    {
+        return $this->daily_price;
+    }
+
+    public function setDailyPrice(float $daily_price): static
+    {
+        $this->daily_price = $daily_price;
+
+        return $this;
+    }
+
+    public function getMonthlyPrice(): ?float
+    {
+        return $this->monthly_price;
+    }
+
+    public function setMonthlyPrice(float $monthly_price): static
+    {
+        $this->monthly_price = $monthly_price;
+
+        return $this;
+    }
+
+    public function isTransmission(): ?bool
+    {
+        return $this->transmission;
+    }
+
+    public function setTransmission(bool $transmission): static
+    {
+        $this->transmission = $transmission;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): static
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+}
