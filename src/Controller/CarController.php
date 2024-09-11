4fda9dc8254597d12c1579af79f7e3da12470680
Kerkar <kerkar.kevin@gmail.com>
2024-09-12 01:17:14 +0200

    Create Car controller and page added

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Car;
+use App\Form\AddCarType;
 use App\Repository\CarRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -32,18 +34,31 @@ class CarController extends AbstractController
     }
 
     #[Route('/deleteCar/{id}', name: 'deleteCar')]
-    public function deleteCar(EntityManagerInterface $manager, $id): Response
+    public function deleteCar(EntityManagerInterface $manager, Car $car): Response
     {
         $repo = $manager->getRepository(Car::class);
-        $car = $repo->find($id);
         $manager->remove($car);
         $manager->flush();
         return $this->redirectToRoute('home');
     }
 
     #[Route('/createCar', name: 'createCar')]
-    public function createCar(EntityManagerInterface $manager): Response
-    {
-        return $this->redirectToRoute('home');
+    public function createCar(
+        EntityManagerInterface $manager,
+        Request $request
+    ): Response {
+        $form = $this->createForm(AddCarType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $car = $form->getData();
+            $manager->persist($car);
+            $manager->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('car/createCar.html.twig', [
+            'controller_name' => 'CarController',
+            'form' => $form->createView(),
+        ]);
     }
 }
